d4253178cb1d1052a0f8424fd8c57170af0ff145
Selim Cifci <cifcihasanselim@gmail.com>
2025-09-21 20:24:33 +0200

    added free_list function to avoid memory leaks

--- main.c
+++ main.c
@@ -8,6 +8,8 @@ typedef struct node
 } node_t;
 
 node_t *iterate(node_t *head, int index) {
+    if (head == NULL) return NULL;
+
     node_t *current = head;
 
     int current_index = 0;
@@ -23,8 +25,8 @@ node_t *iterate(node_t *head, int index) {
 void add_node(node_t **head, int index, int data) {
     node_t *current = iterate(*head, index);
 
-    if(index == 0) {
-        node_t *new = (node_t *)malloc(sizeof(node_t));
+    if (index == 0) {
+        node_t *new = malloc(sizeof(node_t));
 
         new->data = data;
         new->link = *head;
@@ -32,11 +34,11 @@ void add_node(node_t **head, int index, int data) {
     }
     else {
         node_t *previous = iterate(*head, index-1);
-        node_t *new = (node_t *)malloc(sizeof(node_t));
+        node_t *new = malloc(sizeof(node_t));
 
         new->data = data;
         
-        if(previous->link == NULL) {
+        if (previous->link == NULL) {
             new->link = NULL;
         }
         else {
@@ -50,7 +52,7 @@ void add_node(node_t **head, int index, int data) {
 void pop_node(node_t **head, int index) {
     node_t *current = iterate(*head, index);
 
-    if(index == 0) {
+    if (index == 0) {
         *head = current->link;
     }
     else {
@@ -64,7 +66,7 @@ node_t *convert(node_t *head, int arr[], int len) {
     head->data = arr[0];
     head->link = NULL;
 
-    for(int i = 1; i < len; i++) {
+    for (int i = 1; i < len; i++) {
         add_node(&head, i, arr[i]);
     }
 
@@ -74,16 +76,24 @@ node_t *convert(node_t *head, int arr[], int len) {
 void print(node_t *head) {
     node_t *current = head;
 
-    printf("%d\n", *head);
-
-    while (current->link != NULL) {
+    while (current != NULL) {
+        printf("%d\n", current->data);
         current = current->link;
-        printf("%d\n", *current);
+    }
+}
+
+void free_list(node_t *head) {
+    node_t *current = head;
+    
+    while (current != NULL) {
+        node_t *next = current->link;
+        free(current);
+        current = next;
     }
 }
 
 int main() {
-    node_t *head = (node_t *)malloc(sizeof(node_t));
+    node_t *head = malloc(sizeof(node_t));
 
     int arr[] = {5,8,4,1,3,2};
     int length = sizeof(arr)/sizeof(arr[0]);
@@ -92,5 +102,6 @@ int main() {
 
     print(head);
 
+    free_list(head);
     return 0;
 }
